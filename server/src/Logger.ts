263888c677e08c5b9f864f4e9888c0920000dfa0
Luke Gibson <luke.e.gibson@outlook.com>
2026-02-06 01:53:29 +0000

    file logger

--- server/src/Logger.ts
+++ server/src/Logger.ts
@@ -1,10 +1,21 @@
-import express from 'express';
+import express from 'express'; 
+import fs from 'fs'; 
 
 export class Logger {
     private _name: string;
+    private fileStream?: fs.WriteStream;
+    private filePath?: string;
+    private fileLoggingEnabled: boolean = false;
     
-    constructor(name: string = "Root") {
+    constructor(name: string = "Root", loggerConfig?: { output: Array<'console' | 'file'>, file?: { path: string } }) {
         this._name = name;
+        if(loggerConfig) {
+            if(loggerConfig.output.includes("file") && loggerConfig.file) {
+                this.filePath = loggerConfig.file.path;
+                this.fileStream = fs.createWriteStream(this.filePath, { flags: 'a' });
+                this.fileLoggingEnabled = true;
+            }
+        }
     }
 
     public createSublogger(name: string): Logger {
@@ -12,22 +23,30 @@ export class Logger {
     }
 
     public log(message: string, ...args: any[]) {
+        if(this.fileLoggingEnabled && this.fileStream) {
+            const logEntry = `[${new Date().toISOString()}] [${this._name}] ${message} ${args.map(arg => JSON.stringify(arg)).join(' ')}\n`;
+            this.fileStream.write(logEntry);
+        }
         console.log(`[${this._name}] ${message}`, ...args);
     }
 
     public error(message: string, ...args: any[]) {
+        if(this.fileLoggingEnabled && this.fileStream) {
+            const logEntry = `[${new Date().toISOString()}] [${this._name}] ${message} ${args.map(arg => JSON.stringify(arg)).join(' ')}\n`;
+            this.fileStream.write(logEntry);
+        }
         console.error(`[${this._name}] ${message}`, ...args);
     }
 
     public createHttpLogger(): HttpLogger {
-        return new HttpLogger(this._name);
+        return new HttpLogger(this._name, { output: this.fileLoggingEnabled ? ["console", "file"] : ["console"], file: this.fileLoggingEnabled ? { path: this.filePath! } : undefined });
     }
 
 }
 
 export class HttpLogger extends Logger { 
-    constructor(name: string = "Http") {
-        super(name);
+    constructor(name: string = "Http", loggerConfig?: { output: Array<'console' | 'file'>, file?: { path: string } }) {
+        super(name, loggerConfig);
     }
 
     public expressMiddleware = (request: express.Request, response: express.Response, next: express.NextFunction) => {
